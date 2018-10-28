14b37591a32ad5e583371d6cff95be896b06d7ba
Ed Schouten <ed.schouten@prodrive-technologies.com>
2018-10-28 14:29:46 +0100

    Add testing coverage for successful merkeBlobAccess calls.

--- pkg/blobstore/merkle_blob_access_test.go
+++ pkg/blobstore/merkle_blob_access_test.go
@@ -3,6 +3,7 @@ package blobstore
 import (
 	"bytes"
 	"context"
+	"io"
 	"io/ioutil"
 	"testing"
 
@@ -12,8 +13,67 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestMerkleBlobAccessSuccess(t *testing.T) {
+	ctrl, ctx := gomock.WithContext(context.Background(), t)
+	defer ctrl.Finish()
+
+	testSuccess := func(digest *remoteexecution.Digest, body []byte) {
+		// All calls are expect to go to the backend.
+		bottomBlobAccess := mock.NewMockBlobAccess(ctrl)
+		bottomBlobAccess.EXPECT().Get(
+			ctx, "windows10", digest,
+		).Return(ioutil.NopCloser(bytes.NewBuffer(body)))
+		bottomBlobAccess.EXPECT().Put(
+			ctx, "fedora28", digest, int64(len(body)), gomock.Any(),
+		).DoAndReturn(func(ctx context.Context, instance string, digest *remoteexecution.Digest, sizeBytes int64, r io.ReadCloser) error {
+			buf, err := ioutil.ReadAll(r)
+			require.NoError(t, err)
+			require.Equal(t, body, buf)
+			require.NoError(t, r.Close())
+			return nil
+		})
+		bottomBlobAccess.EXPECT().Delete(
+			ctx, "ubuntu1804", digest,
+		).Return(nil)
+		bottomBlobAccess.EXPECT().FindMissing(
+			ctx, "solaris11", []*remoteexecution.Digest{digest},
+		).Return([]*remoteexecution.Digest{digest}, nil)
+
+		blobAccess := NewMerkleBlobAccess(bottomBlobAccess)
+
+		r := blobAccess.Get(ctx, "windows10", digest)
+		buf, err := ioutil.ReadAll(r)
+		require.NoError(t, err)
+		require.Equal(t, body, buf)
+		require.NoError(t, r.Close())
+
+		require.NoError(t, blobAccess.Put(
+			ctx, "fedora28", digest,
+			int64(len(body)), ioutil.NopCloser(bytes.NewBuffer(body))))
+
+		require.NoError(t, blobAccess.Delete(ctx, "ubuntu1804", digest))
+
+		missing, err := blobAccess.FindMissing(
+			ctx, "solaris11", []*remoteexecution.Digest{digest})
+		require.NoError(t, err)
+		require.Equal(t, []*remoteexecution.Digest{digest}, missing)
+	}
+	testSuccess(&remoteexecution.Digest{
+		Hash:      "8b1a9953c4611296a827abf8c47804d7",
+		SizeBytes: 5,
+	}, []byte("Hello"))
+	testSuccess(&remoteexecution.Digest{
+		Hash:      "a54d88e06612d820bc3be72877c74f257b561b19",
+		SizeBytes: 14,
+	}, []byte("This is a test"))
+	testSuccess(&remoteexecution.Digest{
+		Hash:      "1d1f71aecd9b2d8127e5a91fc871833fffe58c5c63aceed9f6fd0b71fe732504",
+		SizeBytes: 16,
+	}, []byte("And another test"))
+}
+
 func TestMerkleBlobAccessMalformedDigests(t *testing.T) {
-	ctrl := gomock.NewController(t)
+	ctrl, ctx := gomock.WithContext(context.Background(), t)
 	defer ctrl.Finish()
 
 	// Perform a series of bad calls with invalid digests. None of
@@ -21,25 +81,24 @@ func TestMerkleBlobAccessMalformedDigests(t *testing.T) {
 	// malformed requests.
 	blobAccess := NewMerkleBlobAccess(mock.NewMockBlobAccess(ctrl))
 	testBadDigest := func(digest *remoteexecution.Digest, errorMessage string) {
-		r := blobAccess.Get(context.Background(), "windows10", digest)
+		r := blobAccess.Get(ctx, "windows10", digest)
 		_, err := ioutil.ReadAll(r)
 		require.Error(t, err)
 		require.Equal(t, errorMessage, err.Error())
 		require.NoError(t, r.Close())
 
 		err = blobAccess.Put(
-			context.Background(), "freebsd12", digest, 5,
+			ctx, "freebsd12", digest, 5,
 			ioutil.NopCloser(bytes.NewBufferString("Hello")))
 		require.Error(t, err)
 		require.Equal(t, errorMessage, err.Error())
 
-		err = blobAccess.Delete(context.Background(), "macos", digest)
+		err = blobAccess.Delete(ctx, "macos", digest)
 		require.Error(t, err)
 		require.Equal(t, errorMessage, err.Error())
 
 		_, err = blobAccess.FindMissing(
-			context.Background(), "debian8",
-			[]*remoteexecution.Digest{digest})
+			ctx, "debian8", []*remoteexecution.Digest{digest})
 		require.Error(t, err)
 		require.Equal(t, errorMessage, err.Error())
 	}
@@ -61,5 +120,4 @@ func TestMerkleBlobAccessMalformedDigests(t *testing.T) {
 	}, "Invalid digest size: -42 bytes")
 }
 
-// TODO(edsch): Validate successful requests.
 // TODO(edsch): Test corrupted data.
