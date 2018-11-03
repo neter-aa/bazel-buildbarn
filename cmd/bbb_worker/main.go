a6d52465dc1bda0dd8a23e47e52814c111f91158
Ed Schouten <ed.schouten@prodrive-technologies.com>
2018-11-03 08:04:31 +0100

    Let the worker print log entries containing URLs.
    The protobufs dumped in the log files by the workers contain very little
    information in practice, do to them giving no access to the action,
    command, input root, etc. Now that we have a somewhat decent web
    frontend for browsing the CAS and AC, change this process to simply
    refer to that page instead.

--- cmd/bbb_worker/main.go
+++ cmd/bbb_worker/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	_ "net/http/pprof"
+	"net/url"
 	"os"
 	"syscall"
 	"time"
@@ -16,18 +19,27 @@ import (
 	"github.com/EdSchouten/bazel-buildbarn/pkg/cas"
 	"github.com/EdSchouten/bazel-buildbarn/pkg/proto/scheduler"
 	"github.com/EdSchouten/bazel-buildbarn/pkg/util"
+	"github.com/golang/protobuf/proto"
 	"github.com/grpc-ecosystem/go-grpc-prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func main() {
 	var (
+		browserURLString = flag.String("browser-url", "http://bbb-browser/", "URL of the Bazel Buildbarn Browser, used for making log entries clickable")
 		blobstoreConfig  = flag.String("blobstore-config", "/config/blobstore.conf", "Configuration for blob storage")
 		schedulerAddress = flag.String("scheduler", "", "Address of the scheduler to which to connect")
 	)
 	flag.Parse()
+
+	browserURL, err := url.Parse(*browserURLString)
+	if err != nil {
+		log.Fatal("Failed to parse browser URL: ", err)
+	}
 
 	// Respect file permissions that we pass to os.OpenFile(), os.Mkdir(), etc.
 	syscall.Umask(0)
@@ -74,13 +86,13 @@ func main() {
 
 	// Repeatedly ask the scheduler for work.
 	for {
-		err := subscribeAndExecute(schedulerClient, buildExecutor)
+		err := subscribeAndExecute(schedulerClient, buildExecutor, browserURL)
 		log.Print("Failed to subscribe and execute: ", err)
 		time.Sleep(time.Second * 3)
 	}
 }
 
-func subscribeAndExecute(schedulerClient scheduler.SchedulerClient, buildExecutor builder.BuildExecutor) error {
+func subscribeAndExecute(schedulerClient scheduler.SchedulerClient, buildExecutor builder.BuildExecutor, browserURL *url.URL) error {
 	stream, err := schedulerClient.GetWork(context.Background())
 	if err != nil {
 		return err
@@ -92,9 +104,37 @@ func subscribeAndExecute(schedulerClient scheduler.SchedulerClient, buildExecuto
 		if err != nil {
 			return err
 		}
-		log.Print("Request: ", request)
+
+		// Print URL in log at the start of the request.
+		browserURL.Path = fmt.Sprintf(
+			"/action/%s/%s/%d/",
+			request.InstanceName,
+			request.ActionDigest.Hash,
+			request.ActionDigest.SizeBytes)
+		log.Print("Action: ", browserURL.String())
+
 		response, _ := buildExecutor.Execute(stream.Context(), request)
-		log.Print("Response: ", response)
+
+		if response.Result != nil {
+			// Print the same URL, but with the ActionResult in
+			// Base64 appended to it. This link works, even if the
+			// ActionResult doesn't end up getting cached.
+			data, err := proto.Marshal(response.Result)
+			if err != nil {
+				return err
+			}
+			browserURL.Path = fmt.Sprintf(
+				"/action/%s/%s/%d/%s",
+				request.InstanceName,
+				request.ActionDigest.Hash,
+				request.ActionDigest.SizeBytes,
+				base64.URLEncoding.EncodeToString(data))
+			log.Print("ActionResult: ", browserURL.String())
+		}
+		if s := status.FromProto(response.Status); s.Code() != codes.OK {
+			log.Print("Error: ", s)
+		}
+
 		if err := stream.Send(response); err != nil {
 			return err
 		}
