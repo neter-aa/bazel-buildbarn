0773a48e03243c97a8ab60c3bcd49c122c89572b
Ed Schouten <ed.schouten@prodrive-technologies.com>
2018-11-21 11:00:16 +0100

    Remove the Redis server.
    This turned out to be a failed experiment. Better just use GRPC.

--- cmd/bbb_storage/main.go
+++ cmd/bbb_storage/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"log"
 	"net"
@@ -32,39 +31,22 @@ func main() {
 	}()
 
 	// Storage access.
-	blobAccess, _, err := configuration.CreateBlobAccessObjectsFromConfig(*blobstoreConfig, false)
+	contentAddressableStorageBlobAccess, _, err := configuration.CreateBlobAccessObjectsFromConfig(*blobstoreConfig, false)
 	if err != nil {
 		log.Fatal("Failed to create blob access: ", err)
 	}
-	rs := blobstore.NewRedisServer(blobAccess)
-
-	// Redis server.
-	sock, err := net.Listen("tcp", ":6379")
-	if err != nil {
-		log.Fatal("Failed to create listening socket: ", err)
-	}
-	go func() {
-		for {
-			conn, err := sock.Accept()
-			if err == nil {
-				go rs.HandleConnection(context.Background(), conn)
-			} else {
-				log.Print("Failed to accept incoming connection: ", err)
-			}
-		}
-	}()
 
 	// RPC server.
 	s := grpc.NewServer(
 		grpc.StreamInterceptor(grpc_prometheus.StreamServerInterceptor),
 		grpc.UnaryInterceptor(grpc_prometheus.UnaryServerInterceptor),
 	)
-	remoteexecution.RegisterContentAddressableStorageServer(s, cas.NewContentAddressableStorageServer(blobAccess))
-	bytestream.RegisterByteStreamServer(s, blobstore.NewByteStreamServer(blobAccess, 1<<16))
+	remoteexecution.RegisterContentAddressableStorageServer(s, cas.NewContentAddressableStorageServer(contentAddressableStorageBlobAccess))
+	bytestream.RegisterByteStreamServer(s, blobstore.NewByteStreamServer(contentAddressableStorageBlobAccess, 1<<16))
 	grpc_prometheus.EnableHandlingTimeHistogram()
 	grpc_prometheus.Register(s)
 
-	sock, err = net.Listen("tcp", ":8982")
+	sock, err := net.Listen("tcp", ":8982")
 	if err != nil {
 		log.Fatal("Failed to create listening socket: ", err)
 	}
