8ca5796ade54d6fe0d33cda25cbcb92b4cf9edab
Mickael Carl <mickael.carl@prodrive-technologies.com>
2019-01-02 20:51:35 +0100

    Add temporary directory cleaning capability
    By passing the temp-directory flag, those directories can be cleaned
    prior to executing every single build action.
    
    Fixes: #18

--- cmd/bbb_runner/main.go
+++ cmd/bbb_runner/main.go
@@ -8,15 +8,18 @@ import (
 	"github.com/EdSchouten/bazel-buildbarn/pkg/environment"
 	"github.com/EdSchouten/bazel-buildbarn/pkg/filesystem"
 	"github.com/EdSchouten/bazel-buildbarn/pkg/proto/runner"
+	"github.com/EdSchouten/bazel-buildbarn/pkg/util"
 
 	"google.golang.org/grpc"
 )
 
 func main() {
+	var tempDirectoriesList util.StringList
 	var (
 		buildDirectoryPath = flag.String("build-directory", "/worker/build", "Directory where builds take place")
 		listenPath         = flag.String("listen-path", "/worker/runner", "Path on which this process should bind its UNIX socket to wait for incoming requests through GRPC")
 	)
+	flag.Var(&tempDirectoriesList, "temp-directory", "Temporary directory that should be cleaned up after a build action. Example: /tmp")
 	flag.Parse()
 
 	buildDirectory, err := filesystem.NewLocalDirectory(*buildDirectoryPath)
@@ -24,8 +27,26 @@ func main() {
 		log.Fatal("Failed to open build directory: ", err)
 	}
 
+	env := environment.NewLocalExecutionEnvironment(buildDirectory, *buildDirectoryPath)
+	var runnerServer runner.RunnerServer
+	// When temporary directories need cleaning prior to executing a build
+	// action, attach a series of TempDirectoryCleaningManagers.
+	if len(tempDirectoriesList) > 0 {
+		m := environment.NewSingletonManager(env)
+		for _, d := range tempDirectoriesList {
+			directory, err := filesystem.NewLocalDirectory(d)
+			if err != nil {
+				log.Fatalf("Failed to open temporary directory %#v: %s", d, err)
+			}
+			m = environment.NewTempDirectoryCleaningManager(m, directory)
+		}
+		runnerServer = environment.NewRunnerServer(environment.NewConcurrentManager(m))
+	} else {
+		runnerServer = env
+	}
+
 	s := grpc.NewServer()
-	runner.RegisterRunnerServer(s, environment.NewLocalExecutionEnvironment(buildDirectory, *buildDirectoryPath))
+	runner.RegisterRunnerServer(s, runnerServer)
 
 	sock, err := net.Listen("unix", *listenPath)
 	if err != nil {
